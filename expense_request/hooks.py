dc2255c9186c064761ced06aae3a6b849c719d4b
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-04-07 03:09:08 +0100

    Update hooks.py

--- expense_request/hooks.py
+++ expense_request/hooks.py
@@ -81,7 +81,7 @@ app_license = "MIT"
 
 doc_events = {
 	"Sales Invoice": {
-		"before_save": "expense_request.sinvoice.validate",
+		"on_save": "expense_request.sinvoice.validate",
 	},
 	"Expense Entry": {
 		"on_submit": "expense_request.api.setup",
